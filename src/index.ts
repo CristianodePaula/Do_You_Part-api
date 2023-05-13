aea61f206f9464785c9b29e2fa0a2b36021cc81d
ChrisDiPaulla <christianodipaulla@gmail.com>
2023-05-12 22:42:37 -0300

    Helmet, Morgam and Express-Rate-Limit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import config from "./config";
 
 const PORT = config.port || 5172;
@@ -7,6 +10,24 @@ const PORT = config.port || 5172;
 const app: Application = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(morgan("common"));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10000,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "Multiple IP requests. Try again in an hour.",
+  })
+);
+
 app.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'Hello World 🌍',
