5d1e054dc288d913ea5dcaab250bf1d0917e9b5b
ChrisDiPaulla <christianodipaulla@gmail.com>
2023-05-13 07:25:07 -0300

    Ts-Node-Dev

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-import morgan from "morgan";
-import helmet from "helmet";
-import rateLimit from "express-rate-limit";
-import config from "./config";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import config from './config';
 
 const PORT = config.port || 5172;
 
@@ -11,7 +11,7 @@ const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan("common"));
+app.use(morgan('common'));
 app.use(
   helmet({
     crossOriginResourcePolicy: false,
@@ -24,15 +24,15 @@ app.use(
     max: 10000,
     standardHeaders: true,
     legacyHeaders: false,
-    message: "Multiple IP requests. Try again in an hour.",
+    message: 'Multiple IP requests. Try again in an hour.',
   })
 );
 
 app.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'Hello World 🌍',
-  })
-})
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
